792ebed75699c6e1502147ddfa39a1fa72c27352
nemirmg <156666849+nemirmg@users.noreply.github.com>
2024-02-09 10:20:03 +0000

    add something about functions

--- primer/Program.cs
+++ primer/Program.cs
@@ -2,7 +2,7 @@
 
 // методы - это понятие допустимо только в ООП
 
-// Простая процедура
+// Простая процедура (в отличие от функции НИЧЕГО не возвращает)
 
 // void f() {
 //         Console.WriteLine("Hello, world!");
@@ -25,10 +25,25 @@
 // Console.WriteLine(n);
 
 // передача по ссылке
-void f(ref int n){
-    n = n + 5;
-}
+// void f(ref int n){
+//     n = n + 5;
+// }
 
-int n = 10;
-f(ref n);
-Console.WriteLine(n);
+// int n = 10;
+// f(ref n);
+// Console.WriteLine(n);
+
+// В отличие от процедуры функция ОБЯЗАНА возвращать какой-то тип данных
+// Выведите наибольшее число среди 2-х
+int maxDouble(int x, int y){
+    if (x > y)
+        return x;
+    return y;
+}
+// return - возвращает значение откуда была вызвана функция!!!
+// return - моментально завершает работу функции!!!
+int result = maxDouble(12, 18);
+Console.WriteLine(result);
+Console.WriteLine(maxDouble(-1, 5));
+Console.WriteLine(maxDouble(134, 8));
+Console.WriteLine(maxDouble(-89, 56));
