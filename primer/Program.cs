dd322b5c84ba03de88b1684537d4370b56035d21
nemirmg <156666849+nemirmg@users.noreply.github.com>
2024-02-14 12:53:13 +0000

    задание 3

--- primer/Program.cs
+++ primer/Program.cs
@@ -17,19 +17,75 @@ string str = new string(chars) -> в данном случае не исполь
 ['a', 'b', 'c', 'd'] => "abcd"
 */
 
+// Console.Clear();
+// Console.Write("Кол-во элементов массива: ");
+// int n = int.Parse(Console.ReadLine()!);
+// char[] chars = new char[n];
+// for (int i = 0; i < chars.Length; i++){
+//     Console.Write("Введите элемент массива: ");
+//     chars[i] = char.Parse(Console.ReadLine()!);
+// }
+// Console.WriteLine($"Начальный массив: [{string.Join(", ", chars)}]");
+// Console.WriteLine(string.Join("", chars)); // простой вариант в одну строку
+// // то же самое с помощью цикла
+// string result = "";
+// foreach (char element in chars){
+//     result += element;
+// }
+// Console.WriteLine(result);
+
+//----------------
+/*
+Задание 2.
+
+На основе символов строки (тип string) сформировать массив
+символов (тип char[]). Вывести массив на экран.
+
+Метод строки ToCharArray() не использовать.
+string str = "Hello!"
+char[] characters = str.ToCharArray(); -> в данном случае не использовать
+
+Пример
+"Hello!" => ['H', 'e', 'l', 'l', 'o', '!' ]
+*/
+
+// Console.Clear();
+// Console.Write("Введите строку: ");
+// string str = Console.ReadLine()!;
+// char[] chars = new char[str.Length];
+// for (int i = 0; i < str.Length; i++){
+//     chars[i] = str[i];
+// }
+// Console.WriteLine($"Результат: [{string.Join(", ", chars)}]");
+
+//----------------
+/*
+Задание 3.
+
+Считать строку с консоли, состоящую из латинских
+букв в нижнем регистре. Выяснить, сколько среди
+введённых букв гласных.
+
+Пример
+"hello" => 2
+"world" => 1
+*/
+
+bool checkVowel(char ch, char[] vowels){
+    foreach (char element in vowels){
+        if (ch == element)
+         return true;
+    }
+    return false;
+}
+
 Console.Clear();
-Console.Write("Кол-во элементов массива: ");
-int n = int.Parse(Console.ReadLine()!);
-char[] chars = new char[n];
-for (int i = 0; i < chars.Length; i++){
-    Console.Write("Введите элемент массива: ");
-    chars[i] = char.Parse(Console.ReadLine()!);
+Console.Write("Введите строку: ");
+string str = Console.ReadLine()!;
+char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y' };
+int count = 0;
+for (int i = 0; i < str.Length; i++){
+    if (checkVowel(str[i], vowels))
+        count++;
 }
-Console.WriteLine($"Начальный массив: [{string.Join(", ", chars)}]");
-Console.WriteLine(string.Join("", chars)); // простой вариант в одну строку
-// то же самое с помощью цикла
-string result = "";
-foreach (char element in chars){
-    result += element;
-}
-Console.WriteLine(result);
+Console.WriteLine(count);
