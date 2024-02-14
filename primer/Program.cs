74d6e48ae92e012c522e933031e35756ded72ebb
nemirmg <156666849+nemirmg@users.noreply.github.com>
2024-02-14 14:33:38 +0000

    задача 1

--- primer/Program.cs
+++ primer/Program.cs
@@ -98,4 +98,14 @@ Console.WriteLine(@"Задача 1:
 Создать строку из символов этого массива.
 ");
 
-
+char[,] charMatrix = {
+    { 'H', 'e', 'l', 'l', 'o', ' ' },
+    { 'w', 'o', 'r', 'l', 'd', '!' }
+};
+string str = "";
+for (int i = 0; i < charMatrix.GetLength(0); i++){
+    for (int j = 0; j < charMatrix.GetLength(1); j++){
+        str += charMatrix[i, j];
+    }
+}
+Console.WriteLine(str);
